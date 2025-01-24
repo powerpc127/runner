63e4d0711d5914c6e78d3cda6e619952db8149fe
powerpc127 <crowellpeterm@gmail.com>
2025-01-24 01:20:43 -0600

    added background, ground, and header

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ pygame.init() # Necessary to initialize pygame and the code will not work withou
 
 screen = pygame.display.set_mode((800, 400)) # Sets the size of the window the game runs in
 clock = pygame.time.Clock() # Creates a clock instance which allows us to control framerate
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50) # To write text we need a font. This selects a font and size
 # Use a while True  loop to keep the game running, otherwise the game will close as soon as you open the program
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill("Red") 
+background_surface = pygame.image.load('graphics/Sky.png') # Used to load the backround .png image
+ground_surface = pygame.image.load('graphics/ground.png') # Same method loads the ground texture
+text_surface = test_font.render('My game', False, 'Green')
+
+
 
 while True:
     # Allow for the game to quit
@@ -21,7 +25,10 @@ while True:
             exit() # Runs sys.exit() to prevent the game logic from continuing to try to run
 
     # screen.blit ('block image transfer') publishes a regular surface onto the display surface (see below)
-    screen.blit(test_surface,(350, 100))
+    screen.blit(background_surface,(0, 0)) # Used 'identify' in the cli to find the size of the image
+    screen.blit(ground_surface, (0, 300)) # Set the ground surface height to the end of the Sky image
+    # Order here matters. Big surfaces first, small surfaces last
+    screen.blit(text_surface, (300, 50)) # Prints the text using the loaded font
 
     # Here we draw all our elements and update everything
     pygame.display.update() # updates the the screen display surface. Call it and forget about it
