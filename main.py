4638a06c31922cddb0d1f93e766069b6d4779989
powerpc127 <crowellpeterm@gmail.com>
2025-01-24 23:05:40 -0600

    text box, basic jump, basic collision detection

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ clock = pygame.time.Clock() # Creates a clock instance which allows us to contro
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50) # To write text we need a font. This selects a font and size
 # Use a while True  loop to keep the game running, otherwise the game will close as soon as you open the program
 
-background_surface = pygame.image.load('graphics/Sky.png') # Used to load the backround .png image
-ground_surface = pygame.image.load('graphics/ground.png') # Same method loads the ground texture
-text_surface = test_font.render('My game', False, 'Green') # Generates text:('text', anti-aliasing bool, color)
+background_surface = pygame.image.load('graphics/Sky.png').convert() # Used to load the backround .png image
+ground_surface = pygame.image.load('graphics/ground.png').convert() # Same method loads the ground texture
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos = 600
+text_surface = test_font.render('My game', False, 'Green') # Generates text:('text', anti-aliasing bool, color)
+text_rect = text_surface.get_rect(center = (400, 50))
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # .convert() and .convert_alpha() are both used to improve performance
+# .converrt_alpha() removes the checkered pattern behind an image. Sky and ground don't have that, so no alpha.
+snail_rect = snail_surface.get_rect(bottomleft = (800, 300))
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300)) # Draws the player rectangle on the floor (300 px)
+player_gravity = 0 # This will be overwritten later
 
 while True:
     # Allow for the game to quit
@@ -24,19 +31,43 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit() # If you press the close button, the game will successfully close down
             exit() # Runs sys.exit() to prevent the game logic from continuing to try to run
+        
+    # Input (still in while loop)    
+        if event.type == pygame.KEYDOWN: # Detects if button is pressed
+            if event.key == pygame.K_UP: # Detects if up arrow is pressed. Full button list here: https://www.pygame.org/docs/ref/key.html#key-constants-label
+                player_gravity = -15
+                player_rect.y -= 1
+
+
+# Here we draw all our elements and update everything
 
     # screen.blit ('block image transfer') publishes a regular surface onto the display surface (see below)
     screen.blit(background_surface,(0, 0)) # Used 'identify' in the cli to find the size of the image
     screen.blit(ground_surface, (0, 300)) # Set the ground surface height to the end of the Sky image
-    # Order here matters. Big surfaces first, small surfaces last
-    screen.blit(text_surface, (300, 50)) # Prints the text using the loaded font
+    # Order here matters. Background surfaces first, foreground surfaces last
+    pygame.draw.rect(screen, 'Pink', text_rect) # This highlights the text by displaying a rectangle behind it
+    # ^Inputs are (surface to print on, color, position)
+    screen.blit(text_surface, (text_rect)) # Prints the text using the loaded font
     
-    snail_x_pos -= 4
-    screen.blit(snail_surface, (snail_x_pos, 264))
-    if snail_x_pos < -72:
-        snail_x_pos = 800
+    snail_rect.x -= 4 # Moves the snail image to the left by 4 pixels each frame
+    if snail_rect.right <= 0: snail_rect.left = 800 # If the right side of the snail leaves the screen, redraw it @ 800px
+    screen.blit(snail_surface, (snail_rect)) # Draws the snail over the snail_rect whose position is dictated above
+    
+    #player
+    if player_rect.bottom > 300: # If the player hits the ground too hard and goes below the floor...
+        player_rect.bottom = 300 # ... then their y position is reset to 300. This creates a floor of 300 px.
+    if player_rect.bottom < 300: # If the player is above the ground...
+        player_rect.y += player_gravity # ... they move down by their current gravity value of pixels...
+        player_gravity += 1 # ... and the value of gravity is increased. This is a basic approximation of physics (no terminal velocity).
+    screen.blit(player_surface, (player_rect)) # When creating a rect you can define the initial position in the rect 
 
-    # Here we draw all our elements and update everything
+
+# To learn about mouse clicking, visit https://youtu.be/AY9MnQ4x3zk?si=0HqP7H9ExS0Q9Gjy&t=3998
+    if player_rect.colliderect(snail_rect):
+        print('collision')
+
+    # keys = pygame.key.get_pressed()
+
     pygame.display.update() # updates the the screen display surface. Call it and forget about it
     clock.tick(60) # Sets the *maximum* framerate to 60 fps
 
