037bbee3860d13fbc3298ec7348cd684c839c314
powerpc127 <crowellpeterm@gmail.com>
2025-01-27 14:28:49 -0600

    enemies spawn at intervals with collision and score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from sys import exit
+from random import randint
 
 def display_time(): # A function to measure the length of time the player has been alive. We call this later to make it visible
     current_time = pygame.time.get_ticks() - start_time # Sets the time. Start_time is defined below as the time since I ran the code
@@ -10,11 +11,18 @@ def display_time(): # A function to measure the length of time the player has be
     screen.blit(time_surf, time_rect) # Prints to the screen
     return time_seconds
 
-def snail_jump():
+def obstacle_movement(obstacle_list):
     global score
-    if snail_rect.right <= 0:
-            score += 1 
-            snail_rect.left = 800 # If the right side of the snail leaves the screen, redraw it @ 800px
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+            screen.blit(snail_surf,obstacle_rect)
+            if obstacle_rect.x <= -72:
+                del obstacle_list[0]
+                score += 1
+        return obstacle_list
+    else:
+        return []
 
 pygame.init() # Necessary to initialize pygame and the code will not work without it
 
@@ -29,32 +37,45 @@ game_active = False # This determines if the game is running or there's a game o
 score = 0 # Sets the starting score. Needs to be done before making score_surf so the value is defined for the f string
 start_time = 0
 time = 0 # Both of these ^ reset the timer for restarting the game 
-# Use a while True  loop to keep the game running, otherwise the game will close as soon as you open the program
+obstacle_rect_list = []
 
+# active surfaces
 background_surf = pygame.image.load('graphics/Sky.png').convert() # Used to load the backround .png image
 ground_surf = pygame.image.load('graphics/ground.png').convert() # Same method loads the ground texture
-
 text_surf = test_font.render('Jump the Snail', False, 'Green') # Generates text:('text', anti-aliasing bool, color)
 text_rect = text_surf.get_rect(center = (400, 50)) # Appiles the text to a rectangle for easy positioning
+
+# inactive surfaces
 game_over_surf_1 = test_font.render("Game Over", False, (0, 200, 200)) # Sets text for a game over
 game_over_rect_1 = game_over_surf_1.get_rect(center = (400, 100)) # Applies a rectangle that's centered vertically
 game_over_surf_2 = small_font.render("Press Space to Try Again", False, (0, 200, 200))
 game_over_rect_2 = game_over_surf_2.get_rect(center = (400, 300))
 new_game_surf_1 = test_font.render("Welcome to Jump the Snail", False, (0, 200, 200)) # Sets text for a game over
-new_game_rect_1 = game_over_surf_1.get_rect(center = (400, 100)) # Applies a rectangle that's centered vertically
+new_game_rect_1 = game_over_surf_1.get_rect(midleft = (210, 100)) # Applies a rectangle that's centered vertically
 new_game_surf_2 = small_font.render("Press Space to Start", False, (0, 200, 200))
-new_game_rect_2 = game_over_surf_2.get_rect(center = (400, 300))
+new_game_rect_2 = game_over_surf_2.get_rect(midleft = (305, 300))
 
+# Obstacles
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha() # .convert() and .convert_alpha() are both used to improve performance
 # .converrt_alpha() removes the checkered pattern behind an image. Sky and ground don't have that, so no alpha.
 snail_rect = snail_surf.get_rect(bottomleft = (800, 300))
+fly_surf = pygame.image.load("graphics/fly/fly1.png").convert_alpha()
+fly_rect = fly_surf.get_rect(bottomleft = (800, 300))
 
+# player images
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300)) # Draws the player rectangle on the floor (300 px)
 player_gravity = 0 # This will be overwritten later
 player_stand = pygame.image.load("graphics/Player/player_stand.png").convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand, 0,2)
 player_stand_rect = player_stand.get_rect(center = (400, 200))
+
+# timer
+obstacle_timer = pygame.USEREVENT + 1 # Userevent is a self defined event. However pygame uses userevent[0]
+# for its own mechanics. Therefore we set obstacle_timer to userevent+1
+pygame.time.set_timer(obstacle_timer, 1500) # Defining the event to make something operate a timer
+
+# Use a while True  loop to keep the game running, otherwise the game will close as soon as you open the program
 
 while True:
     # Allow for the game to quit
@@ -66,10 +87,13 @@ while True:
     # Input (still in while loop)   
         if game_active: 
             if event.type == pygame.KEYDOWN: # Detects if button is pressed. Full button list here: https://www.pygame.org/docs/ref/key.html#key-constants-label
-                if event.key == pygame.K_UP and player_rect.bottom >= 300: # Detects if up arrow is pressed and if we are on the ground
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300: # Detects if up arrow is pressed and if we are on the ground
                     player_gravity = -20
                     player_rect.y -= 1 # This changes our height by 1 so that we don't meet the criteria for being on the floor and can move
-                
+            
+            if event.type == obstacle_timer:
+                obstacle_rect_list.append(snail_surf.get_rect(bottomright = (randint(900, 1100), 300)))
+
         else: # Cancels out the 'if game_active' line above to account for a game over
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE: # Detects if a key is pressed and if it's space
                 snail_rect.left = 800 # Moves the snail to undo any collision
@@ -85,7 +109,7 @@ while True:
         screen.blit(background_surf,(0, 0)) # Used 'identify' in the cli to find the size of the image
         screen.blit(ground_surf, (0, 300)) # Set the ground surface height to the end of the Sky image
         # Order here matters. Background surfaces first, foreground surfaces last
-        time = display_time()
+        time = f"{display_time():.2f}" # Stores the maximum value of time as a variable to call on the game over screen
         
         pygame.draw.rect(screen, 'Yellow', text_rect) # This highlights the text by displaying a rectangle behind it
         # ^Inputs are (surface to print on, color, position)
@@ -93,10 +117,6 @@ while True:
         
         display_time()
 
-        snail_rect.x -= 4 # Moves the snail image to the left by 4 pixels each frame
-        snail_jump()
-        screen.blit(snail_surf, (snail_rect)) # Draws the snail over the snail_rect whose position is dictated above
-        
         #player
         player_rect.y += player_gravity # The player will move down by their current gravity value of pixels...
         player_gravity += 1 # ... and the value of gravity is increased. This is a basic approximation of physics (no terminal velocity).
@@ -107,10 +127,16 @@ while True:
         score_rect = score_surf.get_rect(left = 650, top = 25)
         screen.blit(score_surf, score_rect)
 
+        # Obstacle Movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
-    # To learn about mouse clicking, visit https://youtu.be/AY9MnQ4x3zk?si=0HqP7H9ExS0Q9Gjy&t=3998, also @ 1:42:00
-        if player_rect.colliderect(snail_rect):    
-            game_active = False
+# To learn about mouse clicking, visit https://youtu.be/AY9MnQ4x3zk?si=0HqP7H9ExS0Q9Gjy&t=3998, also @ 1:42:00
+
+        # collision
+        for obstacle_rect in obstacle_rect_list:
+            if player_rect.colliderect(obstacle_rect):
+                obstacle_rect_list = []   
+                game_active = False
 
     else: # Sets the game over conditions
         screen.fill((94,129,162))
